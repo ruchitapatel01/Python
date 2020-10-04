0c1e9b61e6037a35695f3f6a138af55809490ead
ruchitapatel01 <patelruchita012000>
2020-10-04 12:15:15 +0530

    4-10 commit

---
+++ OccurrencesOfWordsInFile.py
@@ -0,0 +1,21 @@
+# a text file is given with 10 lines
+# read that file
+# find occurrences of all the possible words
+
+file = open("TextFile.txt")
+dictionary = dict()
+values = 0
+
+for line in file:
+    for words in line.split():
+        if words in dictionary.keys():
+            dictionary[words] = dictionary[words] + 1
+            if dictionary[words] > values:
+                values = dictionary[words]
+        else:
+            dictionary[words] = 1
+print(dictionary)
+
+for key,val in dictionary.items():
+    if val == values:
+        print(key,val)
