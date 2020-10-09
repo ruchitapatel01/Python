2fdca84963d251708fca0fb1f1d057ddcb3ad849
ruchitapatel01 <patelruchita012000>
2020-10-09 14:48:10 +0530

    9-10 commit

--- OccurrencesOfWordsInFile.py
+++ OccurrencesOfWordsInFile.py
@@ -1,28 +1,30 @@
 # a text file is given with 10 lines
 # read that file
 # find occurrences of all the possible words
+# Modify ur unique words code for 100 files
+# For those 100 files I want all unique words stored into same files.
 
-file = open("TextFile.txt")
-dictionary = dict()
-result = []
+import os
+import re
 
-for line in file:
-    for words in line.split():
-        if words in dictionary.keys():
-            dictionary[words] = dictionary[words] + 1
-        else:
-            dictionary[words] = 1
+for file_name in os.listdir("text_file_bunch"):
+    file = open("text_file_bunch\\" + file_name)
+    dictionary = dict()
+    result = []
 
-for key,val in dictionary.items():
-    if val == 1:
-        result.append(key)
+    for line in file:
+        for words in line.split():
+            words = re.sub('[^A-Za-z0-9]+', '', words)
+            if words in dictionary.keys():
+                dictionary[words] = dictionary[words] + 1
+            else:
+                dictionary[words] = 1
 
-file.close()
+    for key,val in dictionary.items():
+        if val == 1:
+            result.append(key)
+    file.close()
 
-file = open("TextFile.txt","a")
-file.write("\n" + str(result)),
-file.close()
-
-# for key,val in dictionary.items():
-#     if val == values:
-#         print(key,val)
+    file = open("text_file_bunch\\" + file_name,"a")
+    file.write("\n" + str(result)),
+    file.close()
